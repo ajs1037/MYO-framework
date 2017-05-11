676415bef61ea7f617ff9e7448694e7647c06f0c
Adolfo Segura <ajs1037@sru.edu>
2017-05-11 06:05:20 -0400

    Update HelloMyo.java

--- src/main/java/com/thalmic/myo/example/HelloMyo.java
+++ src/main/java/com/thalmic/myo/example/HelloMyo.java
@@ -4,12 +4,15 @@ import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
 
+//This class is the Sample code they provide in the Myo SDK. 
 public class HelloMyo {
 	public static void main(String[] args) {
 		try {
+			//As you can see the a new Hub is created
 			Hub hub = new Hub("com.example.hello-myo");
 
 			System.out.println("Attempting to find a Myo...");
+			//Then a new Myo is created
 			Myo myo = hub.waitForMyo(10000);
 
 			if (myo == null) {
@@ -17,9 +20,10 @@ public class HelloMyo {
 			}
 
 			System.out.println("Connected to a Myo armband!");
+			//Then you have to add the dataCollector onto the hub.... or Myo?.... cunfusing right?
 			DeviceListener dataCollector = new DataCollector();
-			hub.addListener(dataCollector);
-
+			hub.addListener(dataCollector);				//That it is more efficient to just create a new BluetoothDevice
+										// that I made
 			while (true) {
 				hub.run(1000 / 20);
 				System.out.print(dataCollector);
